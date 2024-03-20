ffa4ad2ca8c4ac36ffbf26808ff25cb73e608682
e2469161 <e2469161@ceng.metu.edu.tr>
2024-03-21 01:38:12 +0300

    Implemented template and configured static.

--- Cengden/marketplace/views.py
+++ Cengden/marketplace/views.py
@@ -3,11 +3,11 @@ import time
 from pymongo import MongoClient
 import os
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("<h1>Hello Cengden project!</h1>")
-
+    return render(request, 'index.html')
 
 client = MongoClient('mongodb+srv://atestoygar:leamk123@cengden.gcx1t1k.mongodb.net/')
 
